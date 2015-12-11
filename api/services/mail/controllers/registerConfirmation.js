782e551d8ea5201b53ef3c43acc3a7112bd54e5d
Vitalii Savchuk <esvit666@gmail.com>
2015-12-11 10:06:55 +0200

    init

--- api/services/mail/controllers/registerConfirmation.js
+++ api/services/mail/controllers/registerConfirmation.js
@@ -6,6 +6,8 @@ exports.controller = function (app, sendOpts, model, next) {
     tokenArr.push(model.activationToken[i]);
   }
   model.brokenToken = tokenArr.join('&#8203;');
+  model.activateLink = app.config.get('config.apiUrl') + '/auth/activate/' + model.activationToken;
+  model.activateBrokenLink = app.config.get('config.apiUrl') + '/auth/activate/' + model.brokenToken;
   sendOpts.subject = 'Регистрация аккаунта';
   next(null, model);
 };
