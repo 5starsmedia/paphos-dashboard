30eb11a31a8c268aaae32c67d0ecc0c531aa33c3
Vitalii Savchuk <esvit666@gmail.com>
2016-04-15 12:26:33 +0300

    add analytics

--- api/routes/services.js
+++ api/routes/services.js
@@ -5,20 +5,20 @@ var router = require('express').Router(),
   request = require('request');
 
 router.post('/subscribe', function (req, res, next) {
-  if (!req.body || !req.body.moduleUrl) {
-    var errorMsg = "Request is empty or not have moduleUrl.";
+  if (!req.body || !req.body.pingUrl) {
+    var errorMsg = "Request is empty or not have pingUrl.";
     return next(errorMsg);
   }
 
   async.auto({
     checkRecordExist: function (next) {
-      req.app.models.services.findOne({$or: [{moduleUrl: req.body.moduleUrl}, {name: req.body.name}]}, function (err, record) {
+      req.app.models.services.findOne({$or: [{pingUrl: req.body.pingUrl}, {name: req.body.name}]}, function (err, record) {
         if (err) {
           return next(err);
         }
 
         if (record) {
-          return next('Service with moduleUrl: "' + req.body.moduleUrl + '" or name: "' + req.body.name + '" is already in DB.');
+          return next('Service with pingUrl: "' + req.body.pingUrl + '" or name: "' + req.body.name + '" is already in DB.');
         }
         next();
       });
@@ -35,7 +35,7 @@ router.post('/subscribe', function (req, res, next) {
           return next(err);
         }
 
-        next(null, "Service with moduleUrl: " + result.moduleUrl + " successfully added.");
+        next(null, "Service with pingUrl: " + item.pingUrl + " successfully added.");
       });
     }],
     ping: ['record', function (next, data) {
